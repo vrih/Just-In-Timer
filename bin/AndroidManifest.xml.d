335d42b446efeb3d3f18fb7b6a4a0826330d642d
Daniel Bowtell <dan@danbowtell.co.uk>
2012-11-06 21:35:54 +0000

    Tidy up binary files

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/dan/workspace/JustInTimer/bin/AndroidManifest.xml : \
-/home/dan/workspace/JustInTimer/AndroidManifest.xml \
--- bin/JustInTimer-release-unsigned.apk.d
+++
@@ -1,5 +0,0 @@
-/home/dan/workspace/JustInTimer/bin/JustInTimer-release-unsigned.apk : \
-/home/dan/workspace/JustInTimer/bin/JustInTimer.ap_ \
-/home/dan/workspace/JustInTimer/bin/classes.dex \
-/opt/Android/android-sdk-linux/tools/support/annotations.jar \
-/home/dan/workspace/JustInTimer/libs/GoogleAdMobAdsSdk-6.1.0.jar \
